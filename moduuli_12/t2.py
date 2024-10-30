4dd5d6aa390e03122d9ee32ba621e834a575bd92
zakke-username <zakkezweyg@gmail.com>
2024-10-30 13:37:04 +0200

    weather t2

--- moduuli_12/t2.py
+++ moduuli_12/t2.py
@@ -1,15 +1,19 @@
 import requests
 import json
 
-api_key = ""
+api_key = "fea95d4fdd054096e1c4b516203b3106"
 
-city = input("Anna kaupungin nimi: ")
+city = input("Enter a city: ")
 
 coords_request = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&limit=1&appid={api_key}"
 coords_response = requests.get(coords_request).json()
 latitude = coords_response[0]["lat"]
 longitude = coords_response[0]["lon"]
 
-weather_request = f"https://api.openweathermap.org/data/3.0/onecall?lat={latitude}&lon={longitude}&appid={api_key}"
+weather_request = f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
 weather_response = requests.get(weather_request).json()
-print(json.dumps(weather_response))
+
+weather = weather_response["weather"][0]["main"]
+temp = weather_response["main"]["temp"]
+
+print(f"Weather in {city}: {weather}, temperature: {temp} °C")
